b6a026b21ad72c927be020f4df05358a86bae1e9
Michael <122654000+MichaDev12@users.noreply.github.com>
2024-09-03 14:30:52 -0500

    Add rainbow option
    When the user click the button, the rainbow option become true and
    starts to draw with random colors using a function. I also change the
    name of the first button for a better understanding.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const container = document.querySelector('.container');
 let limit = 16;
 newGrid();
 
+let normalOption = true;
+let rainbowOption = false;
+
 function newGrid() {
   for (let i = 0; i < limit; i++) {
     const row = document.createElement('div');
@@ -19,14 +22,25 @@ function newGrid() {
 
   squares.forEach((square) => {
     square.addEventListener('mouseover', () => {
-      square.style.backgroundColor = 'black';
+      if (normalOption) {
+        square.style.backgroundColor = 'black';
+      }
+
+      if (rainbowOption) {
+        let r = getRandomInt(255);
+        let g = getRandomInt(255);
+        let b = getRandomInt(255);
+        square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      }
     });
   });
 }
 
-const button = document.querySelector('button');
+const size = document.querySelector('button');
+const normalDraw = document.querySelector('#normal');
+const rainbowDraw = document.querySelector('#rainbow');
 
-button.addEventListener('click', () => {
+size.addEventListener('click', () => {
   let changeSize = prompt('Type a number (1-100).');
   while (changeSize < 1 || changeSize > 100) {
     changeSize = prompt('Error. Type a number again (1-100).');
@@ -37,3 +51,17 @@ button.addEventListener('click', () => {
   }
   newGrid();
 });
+
+normalDraw.addEventListener('click', () => {
+  normalOption = true;
+  rainbowOption = false;
+});
+
+rainbowDraw.addEventListener('click', () => {
+  normalOption = false;
+  rainbowOption = true;
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
