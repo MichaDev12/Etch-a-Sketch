b910daec1fe6691ea4360f81d22d63356a057d98
Michael <122654000+MichaDev12@users.noreply.github.com>
2024-09-03 13:36:06 -0500

    Change grid's size
    I add the button in the html and every time the user click the button
    the program ask the user to type a number. After confirm if the limit of
    the grid's size is ok it changes the number's limit variable and calls a
    function to create the grid again with the new limit.
    I use a while to delete every first child of container until is empty.

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
 const container = document.querySelector('.container');
 
 let limit = 16;
-for (let i = 0; i < limit; i++) {
-  const row = document.createElement('div');
-  row.classList.add('row');
-  container.appendChild(row);
-  for (let j = 0; j < limit; j++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    row.appendChild(square);
+newGrid();
+
+function newGrid() {
+  for (let i = 0; i < limit; i++) {
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+    for (let j = 0; j < limit; j++) {
+      const square = document.createElement('div');
+      square.classList.add('square');
+      row.appendChild(square);
+    }
   }
+
+  const squares = document.querySelectorAll('.square');
+
+  squares.forEach((square) => {
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black';
+    });
+  });
 }
 
-const squares = document.querySelectorAll('.square');
+const button = document.querySelector('button');
 
-squares.forEach((square) => {
-  square.addEventListener('mouseover', () => {
-    square.style.backgroundColor = 'black';
-  });
+button.addEventListener('click', () => {
+  let changeSize = prompt('Type a number (1-100).');
+  while (changeSize < 1 || changeSize > 100) {
+    changeSize = prompt('Error. Type a number again (1-100).');
+  }
+  limit = changeSize;
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  newGrid();
 });
