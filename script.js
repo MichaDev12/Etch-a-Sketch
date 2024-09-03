019bce75e02d49a97926d4a3d5d89feaa08cbe17
Michael <122654000+MichaDev12@users.noreply.github.com>
2024-09-03 17:33:30 -0500

    Add progressive darkening effect
    It has the same logic as rainbow and normal draws. I convert the string
    to number and viceversa to get a change in the opacity, every time the
    mouse over it, it changes 0.1 up to 1.
    I also remove the background color and add the reset grid every time the
    user click on the buttons.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ newGrid();
 
 let normalOption = true;
 let rainbowOption = false;
+let darkeningOption = false;
 
 function newGrid() {
   for (let i = 0; i < limit; i++) {
@@ -24,6 +25,7 @@ function newGrid() {
     square.addEventListener('mouseover', () => {
       if (normalOption) {
         square.style.backgroundColor = 'black';
+        square.style.opacity = 1;
       }
 
       if (rainbowOption) {
@@ -31,6 +33,14 @@ function newGrid() {
         let g = getRandomInt(255);
         let b = getRandomInt(255);
         square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        square.style.opacity = 1;
+      }
+
+      if (darkeningOption) {
+        square.style.background = 'black';
+        let currentSquareOpacity = Number(square.style.opacity);
+        let newOpacity = currentSquareOpacity + 0.1;
+        square.style.opacity = String(newOpacity);
       }
     });
   });
@@ -39,6 +49,7 @@ function newGrid() {
 const size = document.querySelector('button');
 const normalDraw = document.querySelector('#normal');
 const rainbowDraw = document.querySelector('#rainbow');
+const darkeningDraw = document.querySelector('#darkening');
 
 size.addEventListener('click', () => {
   let changeSize = prompt('Type a number (1-100).');
@@ -55,11 +66,32 @@ size.addEventListener('click', () => {
 normalDraw.addEventListener('click', () => {
   normalOption = true;
   rainbowOption = false;
+  darkeningOption = false;
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  newGrid();
 });
 
 rainbowDraw.addEventListener('click', () => {
   normalOption = false;
   rainbowOption = true;
+  darkeningOption = false;
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  newGrid();
+});
+
+darkeningDraw.addEventListener('click', () => {
+  normalOption = false;
+  rainbowOption = false;
+  darkeningOption = true;
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  newGrid();
+
 });
 
 function getRandomInt(max) {
